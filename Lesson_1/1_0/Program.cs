2c9e5ddbcfd1080cb46f744ff8c1bc1e244448ca
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-02-23 11:02:00 +0300

    Task 1_0 ver01

--- Lesson_1/1_0/Program.cs
+++ Lesson_1/1_0/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string s = Console.ReadLine()!;
+int num = int.Parse(s);
+Console.WriteLine(num * num);
