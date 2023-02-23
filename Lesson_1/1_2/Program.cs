d0aff0b96f08831be5e189e70ce33c0498db42c8
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-02-23 11:48:59 +0300

    1_2

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,33 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+if(a == 1)
+{
+    Console.WriteLine("Monday");
+}
+else if (a == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+else if (a == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+else if (a == 4)
+{
+    Console.WriteLine("Thursday");
+}
+else if (a == 5)
+{
+    Console.WriteLine("Friday");
+}
+else if (a == 6)
+{
+    Console.WriteLine("Saturday");
+}
+else if (a == 7)
+{
+    Console.WriteLine("Sunday");
+}
+else
+{
+    Console.WriteLine("False");
+}
