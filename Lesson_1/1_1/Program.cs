d3c8b3ca5ac616ad5e686b4bdbf64703baec82f4
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-02-23 11:23:33 +0300

    kvadrat chisla

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет,
+//    является ли первое число квадратом второго.
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if (a == b * b)
+{
+    Console.WriteLine("Yes");
+
+}
+else
+{
+    Console.WriteLine("No");
+
+}
