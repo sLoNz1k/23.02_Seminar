f0833cb08e802ee63fa4e0369598407e9fef7e95
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-02-24 11:20:30 +0300

    finish

--- Lesson_1/1_3/Program.cs
+++ Lesson_1/1_3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Напишите программу, которая на вход принимает одно число
+//    (N), а на выходе показывает все целые числа в промежутке от -N до N.
+int a = int.Parse(Console.ReadLine()!);
+int b = -a;
+while (b <= a)
+{
+    Console.Write($"{b} ");
+    b ++;
+}
